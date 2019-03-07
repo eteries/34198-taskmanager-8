5e93b47224fe51cf179bdf46ac24453b9fbebd51
Сергей Яньшин <yanshin.s@spb.helix.ru>
2019-03-07 14:14:28 +0300

    move month outside

--- src/modules/common/utils.js
+++ src/modules/common/utils.js
@@ -1,21 +1,22 @@
 export const getRandomInteger = (max) => Math.floor(Math.random() * max);
 
+const months = [
+  `January`,
+  `February`,
+  `March`,
+  `April`,
+  `May`,
+  `July`,
+  `August`,
+  `September`,
+  `October`,
+  `November`,
+  `December`
+];
+
 export const formatDate = (date) => {
   const day = new Date(date).getDate();
   const monthNum = new Date(date).getMonth();
-  const months = [
-    `January`,
-    `February`,
-    `March`,
-    `April`,
-    `May`,
-    `July`,
-    `August`,
-    `September`,
-    `October`,
-    `November`,
-    `December`
-  ];
 
   return `${day} ${months[monthNum]}`;
 };

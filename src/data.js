541981cc48fc33f06e25309117f83e0804df2425
Сергей Яньшин <yanshin.s@spb.helix.ru>
2019-03-07 14:06:40 +0300

    sets -> arrays

--- src/data.js
+++ src/data.js
@@ -7,21 +7,19 @@ const taskTitles = [
 const MAX_TAGS_NUM = 3;
 const RECENT_WEEKS_NUM = 2;
 
-export const colors = new Set([`black`, `yellow`, `blue`, `green`, `pink`]);
+export const colors = [`black`, `yellow`, `blue`, `green`, `pink`];
 
-const tags = new Set([`homework`, `theory`, `practice`, `intensive`, `keks`, `doNotForget`]);
+const tags = [`homework`, `theory`, `practice`, `intensive`, `keks`, `doNotForget`];
 
-const days = new Set(
-    [
-      {label: `md`, checked: true},
-      {label: `tu`, checked: false},
-      {label: `we`, checked: true},
-      {label: `th`, checked: false},
-      {label: `fr`, checked: false},
-      {label: `sa`, checked: true},
-      {label: `su`, checked: false},
-    ]
-);
+const days = [
+  {label: `md`, checked: true},
+  {label: `tu`, checked: false},
+  {label: `we`, checked: true},
+  {label: `th`, checked: false},
+  {label: `fr`, checked: false},
+  {label: `sa`, checked: true},
+  {label: `su`, checked: false},
+];
 
 const getRandomRecentDate = () => {
   const WEEK = 7 * 24 * 60 * 60 * 1000;
@@ -31,9 +29,9 @@ const getRandomRecentDate = () => {
 export const getTask = () => ({
   title: taskTitles[Math.floor(Math.random() * taskTitles.length)],
   date: getRandomRecentDate(),
-  tags: [...tags].splice(0, Math.ceil(Math.random() * MAX_TAGS_NUM)),
+  tags: tags.splice(0, Math.ceil(Math.random() * MAX_TAGS_NUM)),
   picture: `http://picsum.photos/100/100?r=${Math.random()}`,
-  color: [...colors][Math.floor(Math.random() * colors.size)],
+  color: colors[Math.floor(Math.random() * colors.length)],
   isRepeating: true,
   repeatingDays: days,
   isFavorite: true,
